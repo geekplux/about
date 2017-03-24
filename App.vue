95279e6e5a092a8fb0ffeb5a4fed72a0797ea6d7
geekplux <geekplux@qq.com>
2017-03-25 02:15:49 +0800

    initial

---
+++ App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app" class="app">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+  }
+</script>
+
+<style>
+  html, body, #app {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+  }
+
+</style>
