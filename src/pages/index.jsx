aca0d17378914d641fa2428d167b15b672458238
geekplux <geekplux@gmail.com>
2019-01-17 17:43:44 +0800

    feat: update my projects

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -74,34 +74,32 @@ const Index = () => (
         <Title>Projects</Title>
         <ProjectsWrapper>
           <ProjectCard
-            title="Freiheit"
-            link="https://www.behance.net/gallery/58937147/Freiheit"
+            title="Markvis"
+            link="https://github.com/geekplux/markvis"
             bg="linear-gradient(to right, #D4145A 0%, #FBB03B 100%)"
           >
-            This project is my entry to Adobe's #ChallengeYourPerspective
-            contest.
+            Make visualization in markdown
           </ProjectCard>
           <ProjectCard
-            title="Harry Potter"
-            link="https://www.behance.net/gallery/52915793/Harry-Potter"
+            title="Typing"
+            link="https://github.com/geekplux/hexo-theme-typing"
             bg="linear-gradient(to right, #662D8C 0%, #ED1E79 100%)"
           >
-            I entered the DOCMA 2017 award with this Harry Potter inspired
-            image.
+            A pure and fresh Hexo theme
           </ProjectCard>
           <ProjectCard
-            title="Tomb Raider"
-            link="https://www.behance.net/gallery/43907099/Tomb-Raider"
+            title="d3-experiment"
+            link="https://github.com/geekplux/d3-experiment"
             bg="linear-gradient(to right, #009245 0%, #FCEE21 100%)"
           >
             Recreation of a Tomb Raider Wallpaper (Fan Art)
           </ProjectCard>
           <ProjectCard
-            title="Eagle"
-            link="https://www.behance.net/gallery/38068151/Eagle"
+            title="More"
+            link="https://github.com/geekplux?tab=repositories&type=source"
             bg="linear-gradient(to right, #D585FF 0%, #00FFEE 100%)"
           >
-            A fantasy image manipulation relocating the habitat of wild animals.
+            More open source projects in my GitHub.
           </ProjectCard>
         </ProjectsWrapper>
       </Projects>
